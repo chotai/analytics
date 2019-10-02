d8dc670d4b1648f50a8192b852b46b9bafdc395e
chotai <sunny.chotai@digital.hmrc.gov.uk>
2019-10-02 09:14:03 +0100

    Add analytics endpoint

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,15 +1,41 @@
 package controllers
 
+import java.util.UUID
+
 import javax.inject._
-import play.api._
+import play.api.libs.ws.{DefaultWSProxyServer, WSClient}
 import play.api.mvc._
+
+import scala.concurrent.ExecutionContext
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(wsClient: WSClient, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) {
+
+  private val url = s"https://www.google-analytics.com/batch"
+
+  private val proxyServer = DefaultWSProxyServer(
+    host = "127.0.0.1",
+    port = 3128,
+    principal = Some("squiduser"),
+    password = Some("test")
+  )
+
+  def send(): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
+
+    val request = s"""{"event":"${UUID.randomUUID().toString}"}"""
+
+    val response = wsClient
+      .url(url)
+      .addHttpHeaders("Content-Type" -> "application/json")
+      .withProxyServer(proxyServer)
+      .post(request)
+
+    response.map(r => Results.Status(r.status)(r.body))
+  }
 
   /**
    * Create an Action to render an HTML page.
--- build.sbt
+++ build.sbt
@@ -8,6 +8,7 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.12.8"
 
 
+libraryDependencies += ws
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
 
